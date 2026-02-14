bbabf3e53458c48da2695ade441d91c98bd84be6
Tarunya K <tarunya.programmer@gmail.com>
2026-02-15 05:23:41 +0530

    chore: switch API baseURL to Render deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://echo-fullstackdeplayed-chatnow.onrender.com/api",
 });
 
 // Attach token to every request
